57652bdb209a31f27d85d7209fff41b66fb53d37
Price <205605@carolyn-price-1046.nyhq.nytint.com>
2016-07-12 16:38:04 -0400

    Validation

--- src/app.js
+++ src/app.js
@@ -55,18 +55,24 @@ db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='acronyms'", 
         // If first word is 'help' give list of help items
         var query = pReq.body.text;
         var commandSplit = query.split(' ');
-        var command = commandSplit[0];
-        var acorn = commandSplit[1];
+        var command = commandSplit[0].toUpperCase();
+        var acorn = commandSplit[1].split(',')[0];
         console.log("INITIAL QUERY", query);
-        console.log("COMMAND IS", command.toUpperCase());
-        if (command.toUpperCase() == 'DEFINE') {
+        console.log("COMMAND IS", command);
+
+
+        // DEFINE AN ACRONYM
+        if (command == 'DEFINE') {
             var definition = query.split(',');
-            if (definition.length < 3) {
+            if (definition.length < 2) {
                 pRes.send('Your definition is malformed. Please Try again');
             } else {
                     var acronym = definition[0].split(' ')[1]; // get rid of define
                     var description = definition[1];
-                    var url = definition[2];
+                    var url = '';
+                    if(definition.length === 3) {
+                        var url = definition[2];
+                    }
                     console.log('Acronym to define: ', acronym);
                     var sqlSelect = "SELECT * FROM 'acronyms' WHERE acronym = '" + acronym.toUpperCase() + "'";
                      db.all(sqlSelect, function(err, row) {
@@ -97,6 +103,14 @@ db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='acronyms'", 
                      }); // end duplicate check for acronym insert
             }
 
+        } else if(command == 'HELP') {
+            var helpText = 'Hello my name is acorn :tree: \n' +
+            'I am a simple acronym bot \n' +
+            'To look up an acronym say' +
+            '``` /acorn NYT ```' +
+            'To define an acronym say' +
+            '``` /acorn define NYT, The New York Times, http://www.nytimes.com ```'
+
         } else {
             console.log('acronym lookup');
         var sqlSelect = "SELECT * FROM 'acronyms' WHERE acronym = '" + query.toUpperCase() + "'";
@@ -109,9 +123,12 @@ db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='acronyms'", 
               if(row.length == 0) {
                   pRes.send( query + ' is not defined. \n To define acronym: ```/acorn define NYT, The New York Times, http://www.nytimes.com ```');
             } else {
-                pRes.send('Hey I\'m Acorn! :tree:  \n' + query + " is " + row[0].description +
-                           "\n Check out this URL: " + row[0].url
-                    );
+                var msg = 'Hey I\'m Acorn! :tree:  \n' + query.toUpperCase() + " is " + row[0].description;
+
+                if(url !== '') {
+                    msg = msg + "\n Check out this URL: " + row[0].url;
+                }
+                pRes.send(msg);
             }
         }
     });
